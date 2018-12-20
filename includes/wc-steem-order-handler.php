36633cb7467b38c6eda63b2679de0b8bfd4492f4
sagescrub <noelruiz@gmail.com>
2018-12-19 21:55:42 -0800

    Typo

--- includes/wc-steem-order-handler.php
+++ includes/wc-steem-order-handler.php
@@ -35,7 +35,7 @@ class WC_Steem_Order_Handler {
 
 			<h2 class="woocommerce-steem-order-payment-details__title"><?php _e( 'Steem Payment details', 'wc-steem' ); ?></h2>
 
-			<p class="woocommerce-steem-payment-memo-prompt"><em>If you haven't already completed your payment:</em> Please don't forge to include the <strong>"MEMO"</strong> when making a transfer for this transaction to Steem.</p>
+			<p class="woocommerce-steem-payment-memo-prompt"><em>If you haven't already completed your payment:</em> Please don't forget to include the <strong>"MEMO"</strong> when making a transfer for this transaction to Steem.</p>
 			
 			<table class="woocommerce-table woocommerce-table--steem-order-payment-details shop_table steem_order_payment_details">
 				<tbody>
